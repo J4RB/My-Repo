c508af82465dce1dfb92f8403c0305e6d459f041
JBLarsen2107 <45763482+JBLarsen2107@users.noreply.github.com>
2020-09-23 16:48:34 +0200

    Started space invaders game

---
+++ Space-Invaders/settings.py
@@ -0,0 +1,11 @@
+# game options/settings
+TITLE = "Space Invaders"
+WIDTH = 680
+HEIGHT = 940
+FPS = 60
+FONT_NAME = 'arial'
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BGCOLOR = WHITE
