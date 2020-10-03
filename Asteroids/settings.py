a921ace7ff88d56aa448bfb6a4e8faf89bf88302
JBLarsen2107 <45763482+JBLarsen2107@users.noreply.github.com>
2020-10-03 12:24:47 +0200

    Added Asteroids files again

---
+++ Asteroids/settings.py
@@ -0,0 +1,23 @@
+# game options/settings
+TITLE = "Asteroids"
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+FPS = 60
+FONT_NAME = 'arial'
+
+# player properties
+PLAYER_IMG_WIDTH = 29
+PLAYER_IMG_HEIGHT = 32
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.009
+PLAYER_TURN_RATE = 3
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (15, 15, 15)
+RED = (255, 0, 0)
+BGCOLOR = WHITE
+
+# image properties
+LOGO_WIDHT = 500
+LOGO_HEIGHT = 210
