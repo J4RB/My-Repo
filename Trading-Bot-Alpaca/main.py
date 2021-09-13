2e759375a583c46d82b225de76d26131350dc759
Jacob Bregndahl Larsen <jlarsen2107@gmail.com>
2021-09-13 08:30:58 +0200

    Trading bot progression

--- Trading-Bot-Alpaca/main.py
+++ Trading-Bot-Alpaca/main.py
@@ -1,58 +1,60 @@
 import alpaca_trade_api as tradeapi
-from datetime import datetime, timedelta
 from config import *
-
-api = tradeapi.REST(API_KEY, API_SECRET_KEY, base_url='https://paper-api.alpaca.markets')
-
-# Check if the market is open now.
-clock = api.get_clock()
-print('The market is {}'.format('open.' if clock.is_open else 'closed.'))
-
-# Check when the market opens and close today.
-date = datetime.now()
-calendar = api.get_calendar(start=date, end=date)[0]
-market_open = datetime(2000, 1, 1, calendar.open.hour, calendar.open.minute, calendar.open.second) + timedelta(hours=6)         #Y, M, D is a dummy
-market_close = datetime(2000, 1, 1, calendar.close.hour, calendar.close.minute, calendar.close.second) + timedelta(hours=6)
-time_format = "%H:%M:%S"
-print('Market open at {} and close at {} on {}.'.format(market_open.strftime(time_format), market_close.strftime(time_format), date.date()))
-
+from datetime import datetime, timedelta
 
 class TradingBot:
     def __init__(self):
-        pass
+        self.running = True
+        self.api = tradeapi.REST(API_KEY, API_SECRET_KEY, base_url='https://paper-api.alpaca.markets')
+
+        # Check if the market is open
+        self.clock = self.api.get_clock()
+        print('The market is {}'.format('open' if self.clock.is_open else 'closed'))
+
+        # Check when the market opens and close today
+        self.date = datetime.now()
+        self.calendar = self.api.get_calendar(start=self.date, end=self.date)[0]
+        self.market_open = datetime(2000, 1, 1, self.calendar.open.hour, self.calendar.open.minute, self.calendar.open.second) + timedelta(hours=6)         #Y, M, D is a dummy
+        self.market_close = datetime(2000, 1, 1, self.calendar.close.hour, self.calendar.close.minute, self.calendar.close.second) + timedelta(hours=6)
+        self.time_format = "%H:%M:%S"
+        print('Market open at {} and close at {} on {}'.format(self.market_open.strftime(self.time_format), self.market_close.strftime(self.time_format), self.date.date()))
     
+
+    def buy(self, symbol, qty, order_type, time_in_force):
+        print('Buying {} qty of {} shares!'.format(qty, symbol))
+        self.api.submit_order(
+            symbol = symbol,
+            qty = qty,
+            side = 'buy',
+            type = order_type,
+            time_in_force = time_in_force
+        )
+        #buy('MSFT', 1, 'market', 'gtc')
+
+    def sell(self, symbol, qty, order_type, time_in_force):
+        print('Selling {} qty of {} shares!'.format(qty, symbol))
+        self.api.submit_order(
+            symbol = symbol,
+            qty = qty,
+            side = 'sell',
+            type = order_type,
+            time_in_force = time_in_force
+        )
+        #sell('MSFT', 1, 'market', 'opg')
+
+    def viewOpenOrders(self):
+        open_orders_list = self.api.list_orders(
+            status='open',
+            limit=None,
+            nested=True  # show nested multi-leg orders
+        )
+        open_orders = [o for o in open_orders_list]
+        print(open_orders)
+        #viewOpenOrders()
+
     def run(self):
         pass
 
-
-def create_order(symbol, qty, order_type, time_in_force):
-    print('Buying {} qty of {} shares!'.format(qty, symbol))
-    api.submit_order(
-        symbol = symbol,
-        qty = qty,
-        side = 'buy',
-        type = order_type,
-        time_in_force = time_in_force
-    )
-#create_order('MSFT', 1, 'market', 'gtc')
-
-def sell_order(symbol, qty, order_type, time_in_force):
-    print('Selling {} qty of {} shares!'.format(qty, symbol))
-    api.submit_order(
-        symbol = symbol,
-        qty = qty,
-        side = 'sell',
-        type = order_type,
-        time_in_force = time_in_force
-    )
-#sell_order('MSFT', 1, 'market', 'opg')
-
-def view_open_orders():
-    open_orders_list = api.list_orders(
-        status='open',
-        limit=None,
-        nested=True  # show nested multi-leg orders
-    )
-    open_orders = [o for o in open_orders_list]
-    print(open_orders)
-#view_open_orders()
+t = TradingBot()
+while t.running:
+    t.run()
